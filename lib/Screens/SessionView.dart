66afeeb13bd16885b2625947d9e536ae4836abb4
pickledbees <42310888+pickledbees@users.noreply.github.com>
2018-10-28 20:54:36 +0800

    edits

--- lib/Screens/SessionView.dart
+++ lib/Screens/SessionView.dart
@@ -3,6 +3,8 @@ import 'package:meetpoint/MVC.dart';
 import 'MoreSessionInfoUI.dart';
 import 'package:meetpoint/LocalInfoManagers/LocalSessionManager.dart';
 import 'package:meetpoint/LocalInfoManagers/Entities.dart';
+import 'package:meetpoint/Standards/TravelModes.dart';
+import 'package:meetpoint/Standards/LocationTypes.dart';
 
 class SessionView extends View<SessionController> {
   SessionView(c) : super(controller: c);
@@ -11,41 +13,40 @@ class SessionView extends View<SessionController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(controller.model.session.title),),
-      body: Center(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0,),
         child: Column(
           children: <Widget>[
-            Container(
-              child: Text('Others can join this meeting with the following ID'),
+            //primer bar
+            Widgets.primer(),
+            //id prompt
+            Widgets.sessionIdPrompt(controller.model.session.sessionID),
+            //users joined in session
+            Widgets.usersBar([controller.model.session.users[0].name, controller.model.session.users[1].name]),
+            //map display
+            Widgets.mapsDisplay(),
+            //dropdown menu for preferred location types
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    'Preferred Area:',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+                Container(
+                  child: DropdownButton(
+                    value: controller.model.preferredLocationType,
+                    items: Widgets.dropdownItems(),
+                    onChanged: controller.updatePreferredLocation,
+                  ),
+                ),
+              ],
             ),
-            Container(
-              child: Text(controller.model.session.sessionID),
-            ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(controller.model.session.users[0].name),
-                  Text(controller.model.session.users[1].name),
-                ],
-              ),
-            ),
-            Container(
-              height: 100.0,
-              child: PageView(
-                children: <Widget>[
-                  Text('1'),
-                  Text('2'),
-                  Text('3'),
-                ],
-              ),
-            ),
-            Container(
-              child: Text('buttons'),
-            )
-            //description
-            //members
-            //maps display
-            //parameters widget
           ],
         ),
       ),
@@ -55,6 +56,10 @@ class SessionView extends View<SessionController> {
 
 class SessionController extends Controller<SessionModel> {
   SessionController(m) : super(model: m);
+
+  updatePreferredLocation(val) {
+    model.updatePreferredLocation(val);
+  }
 }
 
 class SessionModel extends Model {
@@ -66,6 +71,102 @@ class SessionModel extends Model {
   }
 
   Session session;
-  String placeholder;
+  String preferredLocationType = LocationTypes.list[0];
 
+  updatePreferredLocation(val) {
+    setViewState(() => preferredLocationType = val);
+  }
+
+}
+
+//for widgets to display
+class Widgets {
+  static Widget primer() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 7.0),
+      child: Text(
+        'Others can join this meeting with the following ID',
+        style: TextStyle(color: Colors.grey),
+      ),
+    );
+  }
+
+  static Widget sessionIdPrompt(String sessionId) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 2.5),
+          child: Text(
+            sessionId,
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.grey
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 2.5),
+          child: Icon(
+            Icons.content_copy,
+            color: Colors.grey,
+          ),
+        ),
+      ],
+    );
+  }
+
+  static Widget usersBar(List<String> users) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),//users row
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 5.0),
+            child: Icon(Icons.person),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 5.0),
+            child: Text('Joined:'),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 20.0),
+            child: Text(users[0]),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text(users[1]),
+          ),
+        ],
+      ),
+    );
+  }
+
+  static Widget mapsDisplay() {
+    return Container(
+      color: Colors.black12,
+      height: 300.0,
+      child: PageView(
+        children: <Widget>[
+          Text('1'),
+          Text('2'),
+          Text('3'),
+        ],
+      ),
+    );
+  }
+
+  static List<DropdownMenuItem> dropdownItems() {
+    List<DropdownMenuItem> items = [];
+    List<String> types = LocationTypes.list;
+    for (String type in types) {
+      DropdownMenuItem item = DropdownMenuItem(
+        child: Text(type),
+        value: type,
+      );
+      items.add(item);
+    }
+    return items;
+  }
 }
---
+++ lib/Standards/LocationTypes.dart
@@ -0,0 +1,10 @@
+abstract class LocationTypes {
+  static List<String> list = [
+    'No Preference',
+    'Hospital',
+    'Library',
+    'Office',
+    'Park',
+    'Restaurant',
+  ];
+}
