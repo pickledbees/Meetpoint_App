05398a5c0d5494f2405fd5a89804d22d658e5f75
pickledbees <42310888+pickledbees@users.noreply.github.com>
2018-10-28 17:19:11 +0800

    edits

--- lib/Screens/SessionView.dart
+++ lib/Screens/SessionView.dart
@@ -20,6 +20,7 @@ class SessionController extends Controller<SessionModel> {
 }
 
 class SessionModel extends Model {
+
   SessionModel(String sessionId) {
     LocalSessionManager.loadSession(sessionId: sessionId);
     session = LocalSessionManager.openSession;
@@ -33,77 +34,4 @@ class SessionModel extends Model {
     ),
   );
 
-}
-
-
-
-
-class SessionViewUI extends StatelessWidget {
-
-  final String sessionId;
-  static Session session;
-
-  SessionViewUI(this.sessionId);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(session.title),),
-      body: Center(
-        child: Column(
-          children: [
-            //SessionIDWidget(),
-            //MembersRowWidget(),
-            //MapDisplayWidget(),
-            ParamFormWidget(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void loadSession() {
-    //request session from server
-  }
-}
-
-class MapDisplayWidget extends StatefulWidget {
-  //use pageView
-  @override
-  _MapDisplayWidget createState() => _MapDisplayWidget();
-}
-
-class _MapDisplayWidget extends State<MapDisplayWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: PageView(
-        scrollDirection:Axis.horizontal,
-        children: <Widget>[
-          //mapwidget1()
-          //mapwidget1()
-          //mapwidget1()
-          //mapwidget1()
-        ],
-      ),
-    );
-  }
-}
-
-class ParamFormWidget extends StatefulWidget {
-  @override
-  _ParamFormWidgetState createState() => _ParamFormWidgetState();
-}
-
-class _ParamFormWidgetState extends State<ParamFormWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        //preferred location widget
-        //user1 widget
-        //user2 widget
-      ],
-    );
-  }
 }
