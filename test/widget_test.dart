947cd46c60abcbe6bafd79580d45dfaadecb7f80
pickledbees <42310888+pickledbees@users.noreply.github.com>
2018-10-31 18:58:28 +0800

    bfgnhfbdvzs

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,11 +3,9 @@
 // provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
-
+/*
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:meetpoint/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
@@ -27,3 +25,5 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 }
+
+*/
